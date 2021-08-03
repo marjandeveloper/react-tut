0d6b9cf12d0b921a2dcf432b301f49fae71f0244
anjaaleksa <marjan1505@gmail.com>
2021-08-03 17:38:22 +0200

    Added build directory

--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { data } from './books'
 import Book from './Book'
+import { greeting } from './testing/testing'
+
 // CSS
 import './index.css'
 
 // setup vars
 
 const BookList = () => {
+  console.log(greeting)
   return (
     <section className='booklist'>
       {data.map((book) => {
